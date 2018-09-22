61e08da99fc3e5e61ee722557ce6874970574750
Artem Demo <artemdemo@gmail.com>
2018-09-22 17:16:38 +0300

    test for 404.js

---
+++ src/pages/__tests__/404.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Page404 from '../404';
+
+jest.mock('../../components/Container/Container');
+
+describe('404', () => {
+    it('should render', () => {
+        const tree = renderer.create(
+            <Page404 />
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
