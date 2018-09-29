80298725a06e14987728f524a7cfd021c2bcf0c5
Artem Demo <artemdemo@gmail.com>
2018-09-29 22:01:39 +0300

    simlpified title

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
 import Helmet from 'react-helmet';
 import Container from '../Container/Container';
 
 class Layout extends React.Component {
     renderHeader() {
-        const { location, metaTitle } = this.props;
-        const rootPath = `${__PATH_PREFIX__}/`;
-
-        if (location.pathname === rootPath) {
-            return (
-                <h1>{metaTitle}</h1>
-            )
-        }
+        const { metaTitle } = this.props;
 
         return (
-            <h3>
-                <Link to={'/'}>
-                    {metaTitle}
-                </Link>
-            </h3>
-        );
+            <h1>{metaTitle}</h1>
+        )
     }
 
     render() {
