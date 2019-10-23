8ea8f9bc275c0ce8a6e686238e3db9fb75369fe1
Artem Demo <artemdemo@gmail.com>
2019-10-23 16:02:57 +0300

    simplified push command

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git add public/\*
 git add .gitignore
 git commit -m "Build blog"
 git subtree split --prefix public -b gh-pages # create a local gh-pages branch containing the splitted output folder
-git push -f origin gh-pages:gh-pages          # force the push of the gh-pages branch to the remote gh-pages branch at origin
+git push -f origin gh-pages                   # force the push of the gh-pages branch to the remote gh-pages branch at origin
 git branch -D gh-pages                        # delete the local gh-pages because you will need it: ref
 git checkout master
 git branch -D master_tmp
