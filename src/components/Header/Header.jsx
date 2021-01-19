2041f90735ea126a1384673d255467ffa927bd80
Artem Demo <artemdemo@gmail.com>
2021-01-19 20:12:29 +0200

    updated Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,95 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Link } from 'gatsby';
+import {Link} from 'gatsby';
 import MainMenu from './MainMenu';
 import Container from '../Container/Container';
-import { locationProp } from '../Layout/layoutProps';
+import {locationProp} from '../Layout/layoutProps';
 import UpdateReadyInfo from '../UpdateReadyInfo/UpdateReadyInfo';
-import * as styles from '../../services/styles';
-
-const HeaderWrapper = styled.div`
-    margin-bottom: 20px;
-`;
-
-const HeaderContainer = styled.div`
-    padding: 15px 0;
-    @media (max-width: 575.98px) {
-        text-align: center;
-    }
-`;
-
-const HeaderItem = styled.div`
-    display: inline-block;
-    vertical-align: middle;
-    ${props => props.logo && `
-        width: 50%;
-        min-width: 280px;
-        font-size: 1.5rem;
-        font-weight: 300;
-        @media (max-width: 575.98px) {
-            width: auto;
-            margin-bottom: 10px;
-        }
-    `}
-    ${props => props.menu && `
-        ${styles.clearFix}
-        width: 50%;
-        @media (max-width: 768px) {
-            width: auto;
-        }
-    `}
-`;
-
-const HeaderItemMenu = styled.div`
-    float: right;
-`;
-
-const HeaderSeparator = styled.div`
-    border-bottom: 1px solid #dee2e6;
-    max-width: 90%;
-    margin: 0 auto;
-`;
-
-const HeaderLogoLink = styled(Link)`
-    color: initial;
-    &:hover {
-        text-decoration: none;
-    }
-`;
+import './Header.css';
 
 const Header = (props) => {
-    const { location, title } = props;
-    return (
-        <HeaderWrapper>
-            <Container>
-                <HeaderContainer>
-                    <HeaderItem logo>
-                        <HeaderLogoLink to='/'>
-                            {title}
-                        </HeaderLogoLink>
-                    </HeaderItem>
-                    <HeaderItem menu>
-                        <HeaderItemMenu>
-                            <MainMenu location={location} />
-                        </HeaderItemMenu>
-                    </HeaderItem>
-                </HeaderContainer>
-                <HeaderSeparator />
-                <UpdateReadyInfo />
-            </Container>
-        </HeaderWrapper>
-    );
+  const {location, title} = props;
+  return (
+    <div className='header-wrapper'>
+      <Container>
+        <div className='header-container'>
+          <div className='header-item header-item__logo'>
+            <Link className='header-logo-link' to='/'>
+              {title}
+            </Link>
+          </div>
+          <div className='header-item header-item__menu'>
+            <div className='header-item-menu'>
+              <MainMenu location={location}/>
+            </div>
+          </div>
+        </div>
+        <div className='header-separator' />
+        <UpdateReadyInfo/>
+      </Container>
+    </div>
+  );
 };
 
 Header.propTypes = {
-    location: locationProp,
-    title: PropTypes.string,
+  location: locationProp,
+  title: PropTypes.string,
 };
 
 Header.defaultProps = {
-    location: {},
-    title: '',
+  location: {},
+  title: '',
 };
 
 export default Header;
