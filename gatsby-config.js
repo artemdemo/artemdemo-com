862a28f35f1ca5b69d3e6580f64ce5d9c32dd38c
Artem Demo <artemdemo@gmail.com>
2021-01-19 20:14:39 +0200

    removed service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,58 +2,58 @@ const title = 'Artem Demo, web dev';
 const author = 'Artem Demo';
 
 module.exports = {
-    siteMetadata: {
-        title,
-        author,
-        description: 'Blog about web development',
-        siteUrl: 'https://artemdemo.com',
+  siteMetadata: {
+    title,
+    author,
+    description: 'Blog about web development',
+    siteUrl: 'https://artemdemo.com',
+  },
+  pathPrefix: '/',
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/posts`,
+        name: 'posts',
+      },
     },
-    pathPrefix: '/',
-    plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // You can also manually mark in your markdown where to stop excerpting.
+        // Any file that does not have the given `excerpt_separator` will fall back to the default pruning method.
+        // @link https://using-remark.gatsbyjs.org/excerpts/
+        excerpt_separator: '<!-- end -->',
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
             options: {
-                path: `${__dirname}/content/posts`,
-                name: 'posts',
+              maxWidth: 590,
+              quality: 90,
             },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
             options: {
-                // You can also manually mark in your markdown where to stop excerpting.
-                // Any file that does not have the given `excerpt_separator` will fall back to the default pruning method.
-                // @link https://using-remark.gatsbyjs.org/excerpts/
-                excerpt_separator: '<!-- end -->',
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 590,
-                            quality: 90,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-responsive-iframe`,
-                        options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`,
-                        },
-                    },
-                    'gatsby-remark-prismjs',
-                    'gatsby-remark-copy-linked-files',
-                    'gatsby-remark-smartypants',
-                ],
+              wrapperStyle: `margin-bottom: 1.0725rem`,
             },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-                trackingId: 'UA-38910005-10',
-            },
-        },
-        `gatsby-plugin-feed`,
-        `gatsby-plugin-offline`,
-        `gatsby-plugin-react-helmet`,
-    ],
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-38910005-10',
+      },
+    },
+    `gatsby-plugin-feed`,
+    `gatsby-plugin-remove-serviceworker`,
+    `gatsby-plugin-react-helmet`,
+  ],
 };
