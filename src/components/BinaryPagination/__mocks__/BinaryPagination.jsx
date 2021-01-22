04e33aec78548a9f7e68b58be6fe91fa6d128dae
Artem Demo <artemdemo@gmail.com>
2021-01-22 13:10:47 +0200

    updated pagination

--- src/components/BinaryPagination/__mocks__/BinaryPagination.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BinaryPagination = props => (
-    <div data-mock='BinaryPagination'>
-        {JSON.stringify(props, null, 2)}
-    </div>
-);
-
-export default BinaryPagination;
